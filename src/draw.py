17dc076af9ead066def07a1b08c31c6dc267a918
runranron <williamrpelton@gmail.com>
2018-06-21 17:55:20 -0500

    complete

--- src/draw.py
+++ src/draw.py
@@ -6,36 +6,38 @@ from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, ColumnData
 from bokeh.palettes import Spectral8
 
 from graph import *
+RANGE = 500
 
 graph_data = Graph()
-graph_data.debug_create_test_data()
-print(graph_data.vertexes)
+graph_data.randomize(4, 5, 100)
+graph_data.get_connected_components()
+print([x.pos for x in graph_data.vertices])
 
-N = len(graph_data.vertexes)
+N = len(graph_data.vertices)
 node_indices = list(range(N))
 
 color_list = []
-for vertex in graph_data.vertexes:
+for vertex in graph_data.vertices:
     color_list.append(vertex.color)
 
-values = [x.value for x in graph_data.vertexes]
+values = [x.value for x in graph_data.vertices]
 debug_pallete = Spectral8
 debug_pallete.append('#ff0000')
 debug_pallete.append('#0000ff')
 
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
-              tools='', toolbar_location=None)
+plot = figure(title='Graph Layout Demonstration', x_range=(0, RANGE), y_range=(0, RANGE),
+                                                        tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.data_source.add(values, 'value')
-graph.node_renderer.glyph = Circle(name='value', radius=10, fill_color='color')
+graph.node_renderer.glyph = Circle(name='value', radius=20, fill_color='color')
 
 start = []
 end = []
-for vertex in graph_data.vertexes:
+for vertex in graph_data.vertices:
     for edge in vertex.edges:
         start.append(vertex.value)
         end.append(edge.destination.value)
@@ -45,15 +47,15 @@ graph.edge_renderer.data_source.data = dict(
     end=end)
 
 ### start of layout code
-x = [v.pos['x'] for v in graph_data.vertexes]
-y = [v.pos['y'] for v in graph_data.vertexes]
+x = [v.pos['x'] for v in graph_data.vertices]
+y = [v.pos['y'] for v in graph_data.vertices]
 
 source = ColumnDataSource(data=dict(x_pos=x,
                                     y_pos=y,
                                     names=values))
 
-labels = LabelSet(x='x_pos', y='y_pos', text='names', level='glyph',
-        x_offset=0, y_offset=20, source=source, render_mode='canvas')
+labels = LabelSet(x='x_pos', y='y_pos', text='names', level='overlay',
+            text_baseline='middle', text_align='center', source=source, render_mode='canvas')
 
 plot.add_layout(labels)
 
--- src/graph.py
+++ src/graph.py
@@ -1,32 +1,106 @@
+import random
+import math
+
 class Edge:
-    def __init__(self, destination):
+    def __init__(self, destination, weight=0):
         self.destination = destination
+        self.weight = weight
 
 class Vertex:
-    def __init__(self, value, color, **pos):
+    def __init__(self, value=0, color='#000000', x=0, y=0):
         self.value = value
         self.color = color
-        self.pos = pos
+        self.pos = {'x':x, 'y':y}
         self.edges = []
 
 class Graph:
     def __init__(self):
-        self.vertexes = []
+        self.vertices = []
 
-    def debug_create_test_data(self):
-        debug_vertex_1 = Vertex(0, 'red', x=40, y=40)
-        debug_vertex_2 = Vertex(1, 'blue', x=140, y=140)
-        debug_vertex_3 = Vertex(2, 'green', x = 300, y = 400)
-        debug_vertex_4 = Vertex(3, 'orange', x = 400, y = 450)
+    def create_test_data(self):
+        vertex_1 = Vertex(0, 'red', 40, 40)
+        vertex_2 = Vertex(1, 'blue', 140, 140)
+        vertex_3 = Vertex(2, 'green', 300, 400)
+        vertex_4 = Vertex(3, 'orange', 400, 450)
 
-        debug_edge_1 = Edge(debug_vertex_2)
-        debug_edge_2 = Edge(debug_vertex_3)
-        debug_edge_3 = Edge(debug_vertex_4)
-        debug_edge_4 = Edge(debug_vertex_1)
+        edge_1 = Edge(vertex_2)
+        edge_2 = Edge(vertex_3)
+        edge_3 = Edge(vertex_4)
+        edge_4 = Edge(vertex_1)
 
-        debug_vertex_1.edges.append(debug_edge_1)
-        debug_vertex_2.edges.append(debug_edge_2)
-        debug_vertex_3.edges.append(debug_edge_3)
-        debug_vertex_4.edges.append(debug_edge_4)
+        vertex_1.edges.append(edge_1)
+        vertex_1.edges.append(edge_3)
+        vertex_2.edges.append(edge_2)
+        vertex_3.edges.append(edge_3)
+        vertex_4.edges.append(edge_4)
         
-        self.vertexes += [debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4]
+        self.vertices += [vertex_1, vertex_2, vertex_3, vertex_4]
+
+
+    def randomize(self, width, height, px_box, probability=0.6):
+        def connect_vertices(v0, v1):
+            v0.edges.append(Edge(v1))
+            v1.edges.append(Edge(v0))
+
+        count = 0
+        
+        # Build a grid of vertices
+
+        grid = []
+        for y in range(height):
+            row = []
+            for x in range(width):
+                vert = Vertex(count)
+                count += 1
+                row.append(vert)
+            grid.append(row)
+        
+        #Go through the grid randomly hooking up edges
+        for y in range(height):
+            for x in range(width):
+                #connect down
+                if (y < height - 1):
+                    if (random.random() < probability):
+                        connect_vertices(grid[y][x], grid[y+1][x])
+                if (x < width - 1):
+                    if (random.random() < probability):
+                        connect_vertices(grid[y][x], grid[y][x+1])
+        box_buffer = 0.8
+        box_inner = px_box * box_buffer
+        box_inner_offset = (px_box - box_inner) / 2
+
+        for y in range(height):
+            for x in range(width):
+                grid[y][x].pos = {
+                'x': (x * px_box + box_inner_offset + random.random() * box_inner),
+                'y': (y * px_box + box_inner_offset + random.random() * box_inner)
+                }
+        
+        # Finally, add everything in our grid to the dddddd in this Graph
+        for y in range(height):
+            for x in range(width):
+                self.vertices.append(grid[y][x])
+
+    def bfs(self, start):
+        random_color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])        
+        queue = []
+        found = []
+        queue.append(start)
+        found.append(start)
+
+        start.color = random_color
+        while len(queue) > 0:
+            v = queue[0]
+            for edge in v.edges:
+                if edge.destination not in found:
+                    found.append(edge.destination)
+                    queue.append(edge.destination)
+                    edge.destination.color = random_color
+            queue.pop(0)
+            print([v.value for v in found])
+
+    def get_connected_components(self):
+        components = []
+        for vertex in self.vertices:
+            if vertex not in components:
+                components.append(self.bfs(vertex))
